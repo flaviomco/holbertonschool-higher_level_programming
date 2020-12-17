8fa8cf105a6708b6f8c81352230da507ce91b7b3
flaviomco <2109@holbertonschool.com>
2020-12-17 16:57:57 +0000

    6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-     [print("{}: {}".format(i, a_dictionary[i])) for i in sorted(a_dictionary)]
+def print_sorted_dictionary(my_dict):
+    if my_dict is None:
+        return
+    for k, v in sorted(my_dict.items()):
+        print("{}: {}".format(k, v))
