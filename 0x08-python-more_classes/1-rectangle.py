002b69b769930b88d24c4f83db8520c9031884b4
flaviomco <2109@holbertonschool.com>
2021-01-11 21:30:15 +0000

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,7 +2,7 @@
 """Defines a class Rectangle"""
 
 
-class Rectangle():
+class Rectangle:
     """private width and height attributes for Rectangle class"""
 
     def __init__(self, width=0, height=0):
@@ -10,28 +10,32 @@ class Rectangle():
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            """get - set width"""
-            return self.__width
+    @property
+    def height(self):
+        """get/set height"""
+        return self.__height
 
-        @width.setter
-        def width(self, value):
-            if not isinstance(value, int):
-                raise TypeError('width must be an integer')
-            if value < 0:
-                raise ValueError('width must be >= 0')
-            self.__width = value
+    @height.setter
+    def height(self, val):
+        if type(val) is int:
+            if val >= 0:
+                self.__height = val
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
 
-        @property
-        def height(self):
-            """get - set height"""
-            return self.__height
+    @property
+    def width(self):
+        """get/set width"""
+        return self.__width
 
-        @height.setter
-        def height(self, value):
-            if not isinstance(value, int):
-                raise TypeError('height must be an integer')
-            if value < 0:
-                raise ValueError('height must be >= 0')
-            self.__height = value
+    @width.setter
+    def width(self, val):
+        if type(val) is int:
+            if val >= 0:
+                self.__width = val
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
