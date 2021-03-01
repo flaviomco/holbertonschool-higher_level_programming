0be850c44e8f1e96b4cec0ab65a06eba3fe7bb44
flaviomco <2109@holbertonschool.com>
2021-03-01 17:39:44 +0000

    10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- script that lists all records of the table second_table
-SELECT SCORE, NAME FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC, name DESC;
