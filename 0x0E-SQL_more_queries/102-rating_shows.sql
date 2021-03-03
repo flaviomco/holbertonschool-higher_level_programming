7ecad7aa57725df6acdf49397b2344771608d55c
flaviomco <2109@holbertonschool.com>
2021-03-03 01:58:37 +0000

    102-rating_shows.sql

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,4 @@
+-- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT a.title, sum(b.rate) AS rating FROM tv_shows a
+       LEFT JOIN tv_show_ratings b ON a.id = b.show_id
+GROUP BY a.title ORDER BY rating DESC, a.title;
