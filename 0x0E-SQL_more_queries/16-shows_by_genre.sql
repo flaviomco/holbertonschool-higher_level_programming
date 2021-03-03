17fc03a7429aee2009f66ba377fc5e53c166dc94
flaviomco <2109@holbertonschool.com>
2021-03-03 00:50:57 +0000

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,4 +2,4 @@
 SELECT a.title, b.name FROM tv_shows AS a
        LEFT JOIN tv_show_genres AS c on a.id = c.show_id
        LEFT JOIN tv_genres AS b on c.genre_id = b.id
-ORDER BY a.title ASC;
+ORDER BY a.title ASC, b.name ASC;
