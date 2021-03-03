2da072a8879c99bb5a91802f289f3de4ed7f88ef
flaviomco <2109@holbertonschool.com>
2021-03-03 01:41:20 +0000

    101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,11 +1,11 @@
 -- comments
-SELECT t.title FROM tv_shows AS t
+SELECT a.title FROM tv_shows AS a
        LEFT JOIN
        	    (SELECT tv_shows.id FROM tv_genres
      	    	    JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
      		    JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-     		    WHERE tv_genres.name = 'Comedy') s1
+     		    WHERE tv_genres.name = 'Comedy') b
 
-	ON t.id = s1.id
-WHERE s1.id IS NULL
-ORDER BY t.title ASC;
+	ON a.id = b.id
+WHERE b.id IS NULL
+ORDER BY a.title ASC;
