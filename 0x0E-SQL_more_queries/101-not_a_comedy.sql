eed72c9481dbdc275ea8d8c2eae06c8d6462db6a
flaviomco <2109@holbertonschool.com>
2021-03-03 01:33:21 +0000

    101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,11 +2,9 @@
 SELECT DISTINCT a.title FROM tv_shows AS a
        LEFT JOIN tv_show_genres AS b ON a.id = b.show_id
        LEFT JOIN tv_genres AS c on b.genre_id = c.id
-WHERE
-	a.title NOT IN
+WHERE a.title NOT IN
 	(SELECT a.title FROM tv_shows as a
 		INNER JOIN tv_show_genres AS b ON a.id = b.show_id
 		INNER JOIN tv_genres AS c on b.genre_id = c.id
-	WHERE
-		c.name = "Comedy")
+	WHERE c.name = "Comedy")
 ORDER BY a.title;
