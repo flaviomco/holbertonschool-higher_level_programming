d083cbf68bf21ac9e1293adeff8a36a73d77766a
flaviomco <2109@holbertonschool.com>
2021-04-09 15:28:44 +0000

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,3 +3,11 @@
 import requests
 from sys import argv
 
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    auth = (argv[1], argv[2])
+    response = requests.get(url, auth=auth)
+    try:
+        print(response.json().get("id"))
+    except ValueError:
+        print("Not a valid JSON")
